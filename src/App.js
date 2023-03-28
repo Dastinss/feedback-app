9898abdb89e6bf6b532132a9a2d3147748d06e5b
Dastinss <sergeyfdaragan@gmail.com>
2023-03-28 03:04:57 +0300

    # 3.5. done

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
       <Header />
       <div className='container'>
         <FeedbackList feedback = {feedback}/>
-        <Card> Hello World </Card>
       </div>
     </>
   );
